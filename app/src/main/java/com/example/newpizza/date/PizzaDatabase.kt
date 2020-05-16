11d48affbe10c4c4cf093d3cecfc324eb2c4a9fe
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-16 19:41:14 +0800

    修正PizzaDatabase 增加抽象方法ToppingDao

--- app/src/main/java/com/example/newpizza/date/PizzaDatabase.kt
+++ app/src/main/java/com/example/newpizza/date/PizzaDatabase.kt
@@ -3,12 +3,14 @@ package com.example.newpizza.date
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Pizza::class],version=1)
+@Database(entities = [Pizza::class,Topping::class],version=1)
 //標示Database裡面必須要有兩個東西，一個是entities(裡面要標示類別的物體)第二個是version的語法
 abstract class PizzaDatabase: RoomDatabase() {
 //    抽象類別的class繼承了RoomDatabase()
     abstract fun pizzaDao() :PizzaDao
 //    抽象方法pizzaDao 繼承了PizzaDao
+    abstract fun toppingDao():ToppingDao
+//    抽象方法toppingDao繼承了ToppingDao
 }
 
 /*原始資料
