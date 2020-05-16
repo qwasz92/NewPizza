f59612d6e236bf56907db4ab7dd6bf2e3d4a5160
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-16 17:31:01 +0800

    建立ToppingDao 抽象的ToppingDao 建立資料庫insert 建立取得全部資料getAll

---
+++ app/src/main/java/com/example/newpizza/date/ToppingDao.kt
@@ -0,0 +1,27 @@
+package com.example.newpizza.date
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ToppingDao{
+    @Insert
+    fun insert(topping: Topping)
+    @Query ("select * from topping")
+    fun getAll()
+}
+/*原始資料內容
+@Dao
+interface ToppingDao {
+
+    @Query("select * from topping")
+    fun getAll(): List<Topping>
+
+    @Query("select * from topping where id = :id")
+    fun getToppingById(id: Int): Topping
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(topping: Topping)
+}
+ */
