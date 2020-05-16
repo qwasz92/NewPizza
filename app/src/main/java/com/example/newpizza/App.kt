459ad758508cc59808e506046bf4020cd308234d
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-16 20:05:13 +0800

    修正物件導向App 建立晚一點才會執行的db App類別繼承Application 創立一個onCreate方法 db資料是從Room裡面的databaseBuilder取得 透過thread執行序取得裡面的資料 而圖檔的取得是透過getBitmap方法來取得圖檔名稱

--- app/src/main/java/com/example/newpizza/App.kt
+++ app/src/main/java/com/example/newpizza/App.kt
@@ -1,7 +1,12 @@
 package com.example.newpizza
 
+import android.app.Application
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.Room
+import com.example.newpizza.date.PizzaDatabase
 import com.example.newpizza.date.Topping
+import kotlin.concurrent.thread
 
 val toppingBitmaps = mutableMapOf<Topping,Bitmap>()
 val toppings = mutableListOf<Topping>(
@@ -14,3 +19,18 @@ val toppings = mutableListOf<Topping>(
     Topping(7,"Pineapple","topping_pineapple"),
     Topping(8,"Spinach","topping_spinach")
 )
+    lateinit var db:PizzaDatabase
+
+class App :Application(){
+   override fun onCreate(){
+       db = Room.databaseBuilder(applicationContext,PizzaDatabase::class.java,"PizzaDatabase").build()
+   thread {
+       toppings.forEach { toppingBitmaps[it] = getBitmap(it.drawableName)
+       db.toppingDao().insert(it)} }
+       super.onCreate()
+   }
+   private fun getBitmap(drawableName:String):Bitmap{
+        val resId = resources.getIdentifier(drawableName,"drawable",packageName)
+        return BitmapFactory.decodeResource(resources,resId)
+    }
+}
