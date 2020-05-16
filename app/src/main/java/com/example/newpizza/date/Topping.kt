dc7e4822b9c80cf07666a6ced8d8fdc4259c2a27
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-16 17:27:30 +0800

    建立Topping @Entity實體位置 建立id值與name值

---
+++ app/src/main/java/com/example/newpizza/date/Topping.kt
@@ -0,0 +1,21 @@
+package com.example.newpizza.date
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Topping (
+    @PrimaryKey
+    val id:Int,
+    val name:String
+)
+/*原資料內容
+@Entity
+data class Topping (
+
+        @PrimaryKey
+    val id: Int,
+    val name: String,
+    val drawableName: String
+)
+ */
