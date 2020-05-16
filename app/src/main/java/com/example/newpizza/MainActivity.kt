43a97ea8604e701767198d27ad1950064ad2e0ba
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-16 14:00:11 +0800

    設計MainActivity首頁畫面 1.建立RecycleLerView的adapter 2.建立FunctionsAdapter繼承RecyclerView.Adapter<FunctionHolder> 3.取得三個方法(onCreateViewHolder、getItemCount、onBindViewHolder 4.FunctionHolder裡面是什麼

--- app/src/main/java/com/example/newpizza/MainActivity.kt
+++ app/src/main/java/com/example/newpizza/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.newpizza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.pizza_row.view.*
 
 class MainActivity : AppCompatActivity() {
+    val functions = listOf<String>("Test1","Test2","Test3")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //RecycleLerView
+        pizzaRecyclerView.layoutManager = LinearLayoutManager(this)
+        pizzaRecyclerView.setHasFixedSize(true)
+        pizzaRecyclerView.adapter = FunctionsAdapter()
+    }
+    inner class FunctionsAdapter():RecyclerView.Adapter<FunctionHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.pizza_row,parent,false)
+            val holder = FunctionHolder(view)
+            return holder
+        }
+
+        override fun getItemCount(): Int {
+        return functions.size
+        }
+
+        override fun onBindViewHolder(holder: FunctionHolder, position: Int) {
+            holder.nameText.text = functions.get(position)
+        }
+
+    }
+    class FunctionHolder(view:View): RecyclerView.ViewHolder(view){
+        var nameText:TextView = view.textView
     }
 }
