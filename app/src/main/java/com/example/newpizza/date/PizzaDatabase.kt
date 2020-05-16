b81a73d93bd39e06f9ad5db2de80816c2acd4d6a
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-16 15:19:35 +0800

    使用抽象類別abstract設計PizzaDatabase內容 使用Database語法建立兩樣東西1.entities 2.version 裡面記得要使用抽象類別並且繼承RoomDatabase 最後建立抽象方法pizzaDao() :PizzaDao (下方有原始資料)

---
+++ app/src/main/java/com/example/newpizza/date/PizzaDatabase.kt
@@ -0,0 +1,22 @@
+package com.example.newpizza.date
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Pizza::class],version=1)
+//標示Database裡面必須要有兩個東西，一個是entities(裡面要標示類別的物體)第二個是version的語法
+abstract class PizzaDatabase: RoomDatabase() {
+//    抽象類別的class繼承了RoomDatabase()
+    abstract fun pizzaDao() :PizzaDao
+//    抽象方法pizzaDao 繼承了PizzaDao
+}
+
+/*原始資料
+@Database(entities = [Pizza::class, PizzaTopping::class, Topping::class], version = 2)
+@TypeConverters(DateConverter::class)
+abstract class PizzaDatabase: RoomDatabase() {
+    abstract fun pizzaDao() : PizzaDao
+    abstract fun pizzaToppingDao() : PizzaToppingDao
+    abstract fun toppingDao() : ToppingDao
+}
+ */
